627a018a9ef42e1dfc57a04b8e63b9cd5d857c58
norcasitas <nico.orcasitas@gmail.com>
2014-08-01 16:15:31 -0300

    guita ya

---
+++ ControlGastosGym/src/modelos/Gasto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import org.javalite.activejdbc.Model;
+
+
+
+/**
+ *
+ * @author alan
+ */
+public class Gasto extends Model{
+    
+}
